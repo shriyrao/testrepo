7c2ac378466dba08cb339dbc54fc2535b2512b84
shriyrao-ba <shriya.rao@ba.com>
2024-08-02 13:23:40 +0530

    first commit

---
+++ Pods/Target Support Files/Pods-ofcworkUITests/Pods-ofcworkUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ofcworkUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ofcworkUITests
+@end
---
+++ ofcwork/ofcworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ofcworkApp.swift
+//  ofcwork
+//
+//  Created by shriya on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ofcworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
