b65126d5f9cc332b0745f693df9ac53b6e167c03
shriyrao <46553762+shriyrao@users.noreply.github.com>
2024-08-12 12:42:18 +0530

    Delete Pods directory

--- Pods/Target Support Files/Pods-ofcwork/Pods-ofcwork-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ofcwork : NSObject
-@end
-@implementation PodsDummy_Pods_ofcwork
-@end
--- Pods/Target Support Files/Pods-ofcworkTests/Pods-ofcworkTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ofcworkTests : NSObject
-@end
-@implementation PodsDummy_Pods_ofcworkTests
-@end
